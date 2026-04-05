d0849b8abcad0f8e010e6a21590108e17d4d368b
azkaainurridho514 <azkaainurridho514@gmail.com>
2026-04-05 18:06:12 +0700

    noting

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import { Html5Qrcode } from 'html5-qrcode';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+// Expose ke window
+window.Html5Qrcode = Html5Qrcode;
+window.Swal = Swal;
+
+console.log('App.js loaded');
+console.log('Html5Qrcode:', typeof Html5Qrcode);
+console.log('Swal:', typeof Swal);
